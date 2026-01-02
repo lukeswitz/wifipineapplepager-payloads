bfa436f1399c0d5959fdc4a9152e067274d922ca
Zeriaklr <108645484+Zeriaklr@users.noreply.github.com>
2026-01-01 23:40:32 -0700

    Create payload.sh

---
+++ library/user/general/Pocket_Terminal/payload.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Title: Pocket Term
+# Description: Pure custom command input.
+# Controls: Up/Down to Scroll
+# Author: Zeriaklr
+# Version: 2.0
+
+# Define and create the LOOT directory
+LOOTDIR=/root/loot/Term
+mkdir -p $LOOTDIR
+
+while true
+do 
+    # Main
+    LOG magenta "Termanal"
+    command=$(TEXT_PICKER "Enter Your Command" "")
+    LOG blue "> $command"
+
+    # Cleaning Up command for logging and saving (replace invalid chars with underscores)
+    good_command=$(echo "$command" | tr '/: ' '_')
+    lootfile=$LOOTDIR/$(date -Is)_$good_command
+
+    LOG "Results will be saved to: $lootfile if specified\n"
+
+    #Shows the output of the command that was entered
+    $command | tee $lootfile | tr '\n' '\0' | xargs -0 -n 1 LOG green
+
+    #Ask to save the out put so that the user can review at a later time and save the out come
+    save=$(NUMBER_PICKER "1 Save, 2 Del" 2)
+
+    if [ "$save" == 1 ]; then
+        LOG green "Saved log in $LOOTDIR \n with a file name of $lootfile"
+    elif [ "$save" == 2 ]; then
+        LOG red "Not saving Log"
+        rm -f "$lootfile"
+    else
+        LOG red "invalid input \n Saved log in $LOOTDIR \n with a file name of $lootfile"
+    fi
+
+    # Breaking the loop if the user doesn't want to run another command
+    end=$(NUMBER_PICKER "1 Next, 2 Exit" 1)
+
+    case $end in
+        1)
+            LOG green "Running again"
+            ;;
+        2)
+            LOG red "Exiting the program"
+            break
+            ;;
+        *)
+            LOG red "Invalid input. Exiting by default"
+            break
+            ;;
+    esac
+done
